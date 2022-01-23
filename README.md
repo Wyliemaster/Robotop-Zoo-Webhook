7e77e1ea4223a731de8ed88a417c486014394cdc
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2022-01-23 22:17:37 +0000

    ready

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Zoo-Secret-Info-Collector
  Quick program to view secret info given by the API Key relic
+
+## Usage
+
+Enter the required Data into `data.json` and then host the bot somewhere
+
+### Data.json
+
+- id
+> Enable developer mode and copy the ID from the user
+
+![id](https://media.discordapp.net/attachments/934924967731146792/934927588978229308/unknown.png)
+
+- token
+
+> visit your zoo profile and open developer tools. Take the token from the cookies table within network
+
+![token](https://media.discordapp.net/attachments/934924967731146792/934928172607230002/unknown.png)
+
+- Webhook_URL
+
+> create a webhook in the integrations tab of channel settings
+
+![webhook](https://media.discordapp.net/attachments/934924967731146792/934928527806050364/unknown.png)
---
+++ src/index.js
@@ -0,0 +1,76 @@
+const axios = require('axios');
+const config = require('../data.json')
+
+class GDRequests {
+    constructor(id, token)
+    {
+        this.id = id;
+        this.token = token;
+    }
+
+    async get_request(endpoint, params, headers)
+    {
+        return await axios.get(endpoint, new URLSearchParams(params), headers)
+    }
+
+    async fetch_secret_info()
+    {
+        let response = await this.get_request(`https://robotop.xyz/api/zoo/${this.id}`, {}, {cookie: this.token})
+        if(response.data.equippedRelic == "API Key")
+            return response.data.secretInfo
+        return "kERR_NOAPIKEY"
+    }
+
+    async check_info()
+    {
+        console.log(`[${Date.now()}] Checking Secret Info`)
+        let message = ''
+        let data = await this.fetch_secret_info() || "kERR_FAILEDREQUEST"
+
+
+        if(typeof data === 'string' || data instanceof String) return data;
+
+        let time = Date.now()
+        if(data.rescueCooldown < time) message += "A new rescue is available\n"
+        if(data.questEnd < time) message += "A quest has finished\n"
+
+        if(message != ''){
+            message += `<@!${config.zoo.id}>`
+            await this.send_webhook(message)
+        }
+        return null
+    }
+
+    async send_webhook(content)
+    {
+        axios.post(config.discord.webhook_URL, {
+            "content": content
+        }, {
+            "content-type": "application/json"
+        });
+    }
+}
+
+
+let req = new GDRequests(config.zoo.id, config.zoo.token)
+console.log(`[${Date.now()}] Starting Loop`)
+const interval = setInterval(async function() { 
+    let err = await req.check_info() 
+    switch(err)
+    {
+        case "kERR_NOAPIKEY":
+            console.log("[ROBOTOP ERROR] API Key Relic not equipeped")
+            break;
+        case "kERR_FAILEDREQUEST":
+            console.log("[ROBOTOP ERROR] API Key Relic not equipeped")
+            break
+    }
+
+    if(err) clearInterval(interval);
+}, 5000)
+
+
+
+
+
+
